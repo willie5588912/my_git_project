40239f97425e158e5e1ca11447b7431ace2f215a
Willie <willie5588912@gmail.com>
2016-08-27 19:23:23 +0800

    add hello stacy

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,4 +5,5 @@ int main()
   std::cout << "hello world" << std::endl;
   std::cout << "hello hell" << std::endl;
   std::cout << "hello willie" << std::endl;
+  std::cout << "hello stacy" << std::endl;
 }
